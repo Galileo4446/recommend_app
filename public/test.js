61d936abb7da82b82c758e22f9d669c859071469
500-git <qq953406331@gmail.com>
2021-02-05 00:36:39 +0900

    prototype of ui

---
+++ public/test.js
@@ -0,0 +1,61 @@
+	var previous_path = new Array();
+
+	var chosen = new Array();
+
+	function chooseImg(id,src){
+
+		element = document.getElementById(id)
+		if (element.src.match("lgton"))
+		{
+			element.src = previous_path[id];
+			chosen[id] = 0;			
+		} 
+		else 
+		{
+			previous_path[id] = src;
+			element.src = "./images/lgton.jpg";
+			chosen[id] = 1;
+		}
+	 }
+
+	function show(){
+		var total;
+		total = "";			
+		for (var i=0;i<=chosen.length;i++){
+			if (chosen[i] == 1){
+			total += String(i);			
+			}			
+		}
+		if (total != ""){
+			alert(total);			
+		}					
+	}
+
+	function getQueryVariable(variable)
+	{
+	    var query = window.location.search.substring(1);
+	    var vars = query.split("&");
+	    for (var i=0;i<vars.length;i++) {
+	        var pair = vars[i].split("=");
+	        if(pair[0] == variable){return pair[1];}
+	    }
+	    return(false);
+	}
+
+
+	var count = getQueryVariable("id");
+	function ShowRecent(){
+		document.getElementById("process").innerHTML = count+"/5";		
+	}
+
+	function Next(){
+		if (count<5){			
+		count = parseInt(count)+1;
+		window.location = "test.html?id="+count;
+		}
+		else
+		{
+		window.location = "end.html";
+		}		
+	}
+	
---
+++ public/test_driver.js
@@ -0,0 +1,10 @@
+var usr_fav = new Array();
+
+
+for(var i=0;i<5;i++){
+
+	usr_fav[i] = i+1;
+	
+}
+
+console.log(usr_fav);
